668332f76cf838a20beac9cbc5c919ae04a1027a
Abhishek Singh <92709741+abhishek-singh-codes@users.noreply.github.com>
2026-01-09 23:40:16 +0530

    Add solution for smallest subtree with deepest nodes
    Implement logic to find the smallest subtree containing all the deepest nodes in a binary tree.

---
+++ Tree/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,36 @@
+/*
+Logic = Calculate height 
+
+if  left height == right height   
+  this node is the answer
+
+if left height > right height : 
+  answwer is in left subtree
+else 
+  answer is in right subtree 
+*/
+
+
+class Solution {
+public:
+    // calculate total depth
+    int getDepth(TreeNode* root){
+        if(root == NULL)return 0;
+        int leftMax = getDepth(root->left);
+        int rightMax = getDepth(root->right);
+        return max(leftMax, rightMax) + 1;
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int leftDepth = getDepth(root->left);
+        int rightDepth = getDepth(root->right);
+        if(leftDepth == rightDepth){
+            return root;
+        }
+        if(leftDepth > rightDepth){
+            return subtreeWithAllDeepest(root -> left);
+        }else{
+            return subtreeWithAllDeepest(root -> right);
+        }
+    }
+};
